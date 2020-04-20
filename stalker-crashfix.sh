acbec5a8f269869f542167273510e705c55269ab
Eric Parsley <56867834+WoofahRayetDev@users.noreply.github.com>
2020-04-20 04:08:38 -0700

    Initial commits

---
+++ stalker-crashfix.sh
@@ -0,0 +1,30 @@
+#!/bin/bash
+
+set -e
+
+cd /home/eric/ 
+cd .steam
+cd steam
+cd steamapps
+cd common
+cd STALKER\ Shadow\ of\ Chernobyl
+cd gamedata
+find -type f -name "Thumbs.db" -exec rm -i {} \;
+cd
+cd /home/ericparsley
+cd .steam
+cd steam
+cd steamapps
+cd common
+cd STALKER\ Clear\ Sky
+cd gamedata
+find -type f -name "Thumbs.db" -exec rm -i {} \;
+cd
+cd /home/eric/ 
+cd .steam
+cd steam
+cd steamapps
+cd common
+cd STALKER\ Call\ of\ Pripyat
+cd gamedata
+find -type f -name "Thumbs.db" -exec rm -i {} \;
