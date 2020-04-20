f146c055c054ad027adebf8cd290145ee8f6048b
Eric Parsley <56867834+WoofahRayetDev@users.noreply.github.com>
2020-04-20 04:12:18 -0700

    Removed stockfish cuz probably wont use it

--- arch-linux-programs.sh
+++ arch-linux-programs.sh
@@ -2,7 +2,7 @@
 
 set -e
 
-yay -s nvidia nvidia-settings lib32-nvidia-utils steam steamcmd discord telegram-desktop ark p7zip unrar obs-studio vlc qbitrorrent retroarch-git retroarch-assets-xmb-git vscodium-bin simplenote-electron-bin spotify minecraft-launcher teamviewer timeshift gamemode woeusb firefox-nightly xdman libreoffice-fresh foxitreader unityhub stockfish pcsx2-git bleachbit gitkraken dotnet-runtime-bin dotnet-host-bin dotnet-sdk-bin thunderbird wine-staging neofetch kdenlive
+yay -s nvidia nvidia-settings lib32-nvidia-utils steam steamcmd discord telegram-desktop ark p7zip unrar obs-studio vlc qbitrorrent retroarch-git retroarch-assets-xmb-git vscodium-bin simplenote-electron-bin spotify minecraft-launcher teamviewer timeshift gamemode woeusb firefox-nightly xdman libreoffice-fresh foxitreader unityhub pcsx2-git bleachbit gitkraken dotnet-runtime-bin dotnet-host-bin dotnet-sdk-bin thunderbird wine-staging neofetch kdenlive
 sudo pacman -Rns $(pacman -Qtdq)
 sudo pacman -Scc
 yay -Scc
