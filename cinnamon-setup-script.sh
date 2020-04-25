83976a4d064afd5fe9136ede719da88934f1fa10
Eric Parsley <56867834+WoofahRayetDev@users.noreply.github.com>
2020-04-25 15:22:48 -0700

    For setting up Cinnamon DE with my usual apps.

---
+++ cinnamon-setup-script.sh
@@ -0,0 +1,4 @@
+#!/bin/bash
+set -e
+
+yay -S steam steamcmd discord telegram-desktop ark p7zip unrar obs-studio vlc qbittorrent kdenlive neofetch wine-staging thunderbird bleachbit libreoffice-fresh lib32-mesa-aco-git mesa-aco-git retroarch-git retroarch-assets-xmb-git visual-studio-code-insiders simplenote-electron-bin spotify minecraft-launcher teamviewer gamemode woeusb xdman foxitreader unityhub pcsx2-git gitkraken dotnet-runtime-bin dotnet-host-bin dotnet-sdk-bin  
