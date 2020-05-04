de672cd3798d969810977489b1bbba876e5294c1
Eric Parsley <56867834+WoofahRayetDev@users.noreply.github.com>
2020-05-03 23:34:01 -0700

    Switched visual studio version so it works better with Unity Game Engine

--- kde-setup-script.sh
+++ kde-setup-script.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 set -e
 
-yay -S cronie steam steamcmd discord telegram-desktop ark p7zip unrar obs-studio vlc qbittorrent libreoffice-fresh bleachbit thunderbird wine-staging neofetch kdenlive lib32-mesa-aco-git mesa-aco-git foxitreader visual-studio-code-insiders retroarch-git retroarch-assets-xmb-git simplenote-electron-bin spotify minecraft-launcher teamviewer timeshift gamemode woeusb xdman unityhub pcsx2-git gitkraken dotnet-runtime-bin dotnet-host-bin dotnet-sdk-bin 
+yay -S cronie steam steamcmd discord telegram-desktop ark p7zip unrar obs-studio vlc qbittorrent libreoffice-fresh bleachbit thunderbird wine-staging neofetch kdenlive lib32-mesa-aco-git mesa-aco-git foxitreader visual-studio-code-bin retroarch-git retroarch-assets-xmb-git simplenote-electron-bin spotify minecraft-launcher teamviewer timeshift gamemode woeusb xdman unityhub pcsx2-git gitkraken dotnet-runtime-bin dotnet-host-bin dotnet-sdk-bin 
 sudo pacman -Rns $(pacman -Qtdq)
 sudo pacman -Scc
 yay -Scc
