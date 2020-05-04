9c128ea9c1ee93ab7b0892c00b722b2330f6f9c1
Eric Parsley <56867834+WoofahRayetDev@users.noreply.github.com>
2020-05-03 21:16:50 -0700

    Powerdevil already installs on laptop. Forgot to add mesa aco git for amd gpu.

--- kde-setup-script.sh
+++ kde-setup-script.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 set -e
 
-yay -S steam steamcmd discord telegram-desktop powerdevil ark p7zip unrar obs-studio vlc qbitrorrent libreoffice-fresh bleachbit thunderbird wine-staging neofetch kdenlive foxitreader visual-studio-code-insiders retroarch-git retroarch-assets-xmb-git simplenote-electron-bin spotify minecraft-launcher teamviewer timeshift gamemode woeusb xdman unityhub pcsx2-git gitkraken dotnet-runtime-bin dotnet-host-bin dotnet-sdk-bin 
+yay -S steam steamcmd discord telegram-desktop ark p7zip unrar obs-studio vlc qbitrorrent libreoffice-fresh bleachbit thunderbird wine-staging neofetch kdenlive lib32-mesa-aco-git mesa-aco-git foxitreader visual-studio-code-insiders retroarch-git retroarch-assets-xmb-git simplenote-electron-bin spotify minecraft-launcher teamviewer timeshift gamemode woeusb xdman unityhub pcsx2-git gitkraken dotnet-runtime-bin dotnet-host-bin dotnet-sdk-bin 
 sudo pacman -Rns $(pacman -Qtdq)
 sudo pacman -Scc
 yay -Scc
