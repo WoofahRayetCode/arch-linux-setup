b4d12a8186f3295eeac4aded3b861f284c721d36
Eric Parsley <56867834+WoofahRayetDev@users.noreply.github.com>
2020-05-13 04:13:55 -0700

    Most likely wont use Cinnamon anymore

--- cinnamon-setup-script.sh
+++
@@ -1,7 +0,0 @@
-#!/bin/bash
-set -e
-
-yay -S steam steamcmd discord telegram-desktop p7zip unrar obs-studio vlc qbittorrent kdenlive neofetch wine-staging thunderbird bleachbit libreoffice-fresh dropbox lib32-mesa-aco-git mesa-aco-git retroarch-git retroarch-assets-xmb-git visual-studio-code-bin simplenote-electron-bin spotify minecraft-launcher teamviewer gamemode woeusb foxitreader pcsx2-git gitkraken dotnet-runtime-bin dotnet-host-bin dotnet-sdk-bin
-sudo pacman -Rns $(pacman -Qtdq)
-sudo pacman -Scc
-yay -Scc
