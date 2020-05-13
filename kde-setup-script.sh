169a522d46698b53b1f02c1d99deaef0714ec9a3
Eric Parsley <56867834+WoofahRayetDev@users.noreply.github.com>
2020-05-13 04:10:02 -0700

    Added player for Pandora Radio

--- kde-setup-script.sh
+++ kde-setup-script.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 set -e
 
-yay -S cronie powerdevil steam discord telegram-desktop ark p7zip unrar obs-studio vlc qbittorrent libreoffice-fresh bleachbit thunderbird wine-staging neofetch kdenlive xscreensaver-arch-logo steamcmd lib32-mesa-aco-git mesa-aco-git foxitreader retroarch-git retroarch-assets-xmb-git pcsx2-git simplenote-electron-bin spotify minecraft-launcher teamviewer timeshift gamemode woeusb xdman gitkraken visual-studio-code-bin unityhub dotnet-runtime-bin dotnet-host-bin dotnet-sdk-bin 
+yay -S cronie gnome-keyring powerdevil steam discord telegram-desktop ark p7zip unrar obs-studio vlc qbittorrent libreoffice-fresh bleachbit thunderbird wine-staging neofetch kdenlive xscreensaver-arch-logo steamcmd lib32-mesa-aco-git mesa-aco-git foxitreader retroarch-git retroarch-assets-xmb-git pcsx2-git simplenote-electron-bin spotify pithos-git minecraft-launcher teamviewer timeshift gamemode woeusb xdman gitkraken visual-studio-code-bin unityhub dotnet-runtime-bin dotnet-host-bin dotnet-sdk-bin 
 sudo pacman -Rns $(pacman -Qtdq)
 sudo pacman -Scc
 yay -Scc
