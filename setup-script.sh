ecd210edadc3064f5e4072e04b35b88743d1d048
Eric Parsley <56867834+WoofahRayetDev@users.noreply.github.com>
2020-06-20 22:32:56 -0700

    Update setup-script.sh

--- setup-script.sh
+++ setup-script.sh
@@ -1,9 +1,8 @@
 #!/bin/bash
 set -e
 
-#Enables all cores for compiling
+#Fully use CPU for compiling
 wget https://raw.githubusercontent.com/WoofahRayetDev/general-linux-stuff/master/all-cpu-cores-compiling.sh
-cd Desktop
 chmod +x all-cpu-cores-compiling.sh
 ./all-cpu-cores-compiling.sh
 
@@ -14,6 +13,13 @@ sudo pacman -S nano wine-staging firefox thunderbird qbittorrent krita bleachbit
 #Command for downloading and setting up Proton Updater
 wget https://raw.githubusercontent.com/flubberding/ProtonUpdater/master/cproton.sh
 chmod +x cproton.sh
+
+#Set up yay
+mkdir Git
+cd Git
+git clone https://aur.archlinux.org/yay.git
+cd yay
+makepkg -si
 
 #Command for programs I use from the AUR
 yay -S retroarch-git retroarch-assets-xmb-git pcsx2-git minecraft-launcher teamviewer xdman pithos-git gamemode lib32-gamemode winetricks protontricks foxitreader davinci-resolve osu-lazer dotnet-sdk-bin dotnet-runtime-bin dotnet-host-bin aspnet-runtime-bin visual-studio-code-bin simplenote-electron-bin
