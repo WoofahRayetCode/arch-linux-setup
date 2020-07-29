e72cf3026aa95f517a1daeec9b1dde978aca6a6b
Eric Parsley <56867834+WoofahRayetDev@users.noreply.github.com>
2020-07-29 12:11:33 -0700

    Update setup-script.sh

--- setup-script.sh
+++ setup-script.sh
@@ -7,14 +7,14 @@ chmod +x all-cpu-cores-compiling.sh
 ./all-cpu-cores-compiling.sh
 
 #Command for main programs I use
-sudo pacman -S nano wine-staging firefox thunderbird qbittorrent krita bleachbit libreoffice-fresh git steam discord telegram-desktop vlc unrar p7zip gparted 
+sudo pacman -S nano wine-staging thunderbird qbittorrent krita bleachbit libreoffice-fresh git steam discord telegram-desktop vlc shotcut unrar p7zip gparted 
 
 #Command for downloading and setting up Proton Updater
 wget https://raw.githubusercontent.com/flubberding/ProtonUpdater/master/cproton.sh
 chmod +x cproton.sh
 
 #Command for programs I use from the AUR
-yay -S retroarch-git retroarch-assets-xmb-git pcsx2-git minecraft-launcher teamviewer xdman pithos-git gamemode lib32-gamemode winetricks protontricks foxitreader davinci-resolve osu-lazer obs-studio-git atom-editor-bin simplenote-electron-bin
+yay -S retroarch-git retroarch-assets-xmb-git pcsx2-git minecraft-launcher teamviewer xdman pithos-git gamemode lib32-gamemode mesa-aco-git lib32-mesa-aco-git winetricks protontricks foxitreader osu-lazer-git obs-studio-git atom-editor-bin simplenote-electron-bin mullvad-vpn-bin
 
 #Commands for cleaning left over files from main program install command and AUR install command.
 sudo pacman -Rns $(pacman -Qtdq)
