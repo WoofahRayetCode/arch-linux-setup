2938ca332baf76908cae6f87e5d22b5b2fc8ed52
Eric Parsley <56867834+WoofahRayetDev@users.noreply.github.com>
2020-09-23 16:34:39 -0700

    Update setup-script.sh

--- setup-script.sh
+++ setup-script.sh
@@ -7,19 +7,21 @@ chmod +x all-cpu-cores-compiling.sh
 ./all-cpu-cores-compiling.sh
 
 #Command for main programs I use
-sudo pacman -S nano wine-staging giflib lib32-giflib libpng lib32-libpng libldap lib32-libldap gnutls lib32-gnutls mpg123 lib32-mpg123 openal lib32-openal v4l-utils lib32-v4l-utils libpulse lib32-libpulse libgpg-error lib32-libgpg-error alsa-plugins lib32-alsa-plugins alsa-lib lib32-alsa-lib libjpeg-turbo lib32-libjpeg-turbo sqlite lib32-sqlite libxcomposite lib32-libxcomposite libxinerama lib32-libgcrypt libgcrypt lib32-libxinerama ncurses lib32-ncurses opencl-icd-loader lib32-opencl-icd-loader libxslt lib32-libxslt libva lib32-libva gtk3 lib32-gtk3 gst-plugins-base-libs lib32-gst-plugins-base-libs vulkan-icd-loader lib32-vulkan-icd-loader lutris lib32-mesa vulkan-radeon lib32-vulkan-radeon vulkan-icd-loader lib32-vulkan-icd-loader thunderbird qbittorrent krita bleachbit libreoffice-fresh git steam discord telegram-desktop vlc shotcut obs-studio unrar p7zip gparted 
+sudo pacman -S nvidia lib32-nvidia-utils nvidia-utils nvidia-settings lib32-mesa vulkan-radeon lib32-vulkan-radeon vulkan-icd-loader lib32-vulkan-icd-loader nano wine-staging thunderbird qbittorrent krita bleachbit libreoffice-fresh git steam discord telegram-desktop vlc shotcut obs-studio unrar p7zip ark gparted 
 
 #Command for downloading and setting up Proton Updater
-wget https://raw.githubusercontent.com/flubberding/ProtonUpdater/master/cproton.sh
+wget https://raw.githubusercontent.com/Termuellinator/ProtonUpdater/master/cproton.sh
 chmod +x cproton.sh
 
 #Clone yay and install
+mkdir Git
+cd Git
 git clone https://aur.archlinux.org/yay.git
 cd yay
 makepkg -si
 
 #Command for programs I use from the AUR
-yay -S retroarch-git retroarch-assets-xmb-git pcsx2-git minecraft-launcher teamviewer xdman pithos-git gamemode lib32-gamemode mesa-aco-git lib32-mesa-aco-git winetricks protontricks foxitreader atom-editor-bin simplenote-electron-bin
+yay -S retroarch-git retroarch-assets-xmb-git pcsx2-git duckstation-git minecraft-launcher teamviewer gamemode lib32-gamemode winetricks protontricks foxitreader visual-studio-code-bin simplenote-electron-bin
 
 #Commands for cleaning left over files from main program install command and AUR install command.
 sudo pacman -Rns $(pacman -Qtdq)
