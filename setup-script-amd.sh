fe55a84913d741649afdf75cd0084ca1f7c60148
Eric Parsley <56867834+WoofahRayetDev@users.noreply.github.com>
2020-10-06 23:37:02 -0700

    Update setup-script-amd.sh

--- setup-script-amd.sh
+++ setup-script-amd.sh
@@ -7,7 +7,7 @@ chmod +x all-cpu-cores-compiling.sh
 ./all-cpu-cores-compiling.sh
 
 #Command for main programs I use
-sudo pacman -S nano wine-staging thunderbird qbittorrent krita bleachbit libreoffice-fresh git steam discord unrar p7zip gparted
+sudo pacman -S nano gamemode lib32-gamemode wine-staging thunderbird qbittorrent krita bleachbit libreoffice-fresh git steam discord unrar p7zip gparted
 
 #Command for downloading and setting up Proton Updater
 wget https://raw.githubusercontent.com/Termuellinator/ProtonUpdater/master/cproton.sh
@@ -22,7 +22,7 @@ makepkg -si
 cd
 
 #Command for programs I use from the AUR
-yay -S mesa-git lib32-mesa-git gamemode lib32-gamemode retroarch-git retroarch-assets-xmb-git pcsx2-git duckstation-git obs-studio-git kdenlive-git minecraft-launcher teamviewer winetricks protontricks foxitreader freetube-bin authy timeshift etcher-bin bitwarden-bin visual-studio-code-bin github-desktop-bin simplenote-electron-bin
+yay -S mesa-git lib32-mesa-git retroarch-git retroarch-assets-xmb-git pcsx2-git duckstation-git obs-studio-git kdenlive-git minecraft-launcher teamviewer winetricks protontricks foxitreader freetube-bin authy timeshift etcher-bin bitwarden-bin visual-studio-code-bin github-desktop-bin simplenote-electron-bin
 
 #Commands for cleaning left over files from main program install command and AUR install command.
 sudo pacman -Rns $(pacman -Qtdq)
