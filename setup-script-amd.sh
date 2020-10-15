7399843236d35ab72fd9f0c7bcd99dbc6837d006
Eric Parsley <56867834+WoofahRayetDev@users.noreply.github.com>
2020-10-15 15:36:33 -0700

    Update setup-script-amd.sh

--- setup-script-amd.sh
+++ setup-script-amd.sh
@@ -7,20 +7,11 @@ chmod +x all-cpu-cores-compiling.sh
 ./all-cpu-cores-compiling.sh
 
 #Command for main programs I use
-sudo pacman -S nano gamemode lib32-gamemode wine-staging thunderbird qbittorrent krita bleachbit libreoffice-fresh git steam discord unrar p7zip gparted
+sudo pacman -S yay nano gamemode lib32-gamemode wine-staging thunderbird qbittorrent krita bleachbit libreoffice-fresh git steam discord unrar p7zip gparted
 
 #Command for downloading and setting up Proton Updater
 wget https://raw.githubusercontent.com/Termuellinator/ProtonUpdater/master/cproton.sh
 chmod +x cproton.sh
-
-#Clone yay and install
-mkdir Git
-cd Git
-git clone https://aur.archlinux.org/yay.git
-cd yay
-makepkg -si
-cd
-
 #Command for programs I use from the AUR
 yay -S mesa-git lib32-mesa-git retroarch-git retroarch-assets-xmb-git pcsx2-git duckstation-git obs-studio-git kdenlive-git minecraft-launcher teamviewer foxitreader timeshift etcher-bin bitwarden-bin github-desktop-bin code-git standardnotes-desktop
 
