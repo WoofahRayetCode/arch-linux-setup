7314e234128505f9d6672a046aaf52f95938163c
WoofahRayetDev <56867834+WoofahRayetDev@users.noreply.github.com>
2020-11-13 16:29:07 -0800

    Added back a couple things

--- setup-script-nvidia.sh
+++ setup-script-nvidia.sh
@@ -32,7 +32,7 @@ wget https://raw.githubusercontent.com/Termuellinator/ProtonUpdater/master/cprot
 chmod +x cproton.sh
 
 #Command for programs I use from the AUR
-yay -S standardnotes-desktop birdtray-git duckstation-git citra-git minecraft-launcher widevine mellowplayer teamviewer foxitreader bitwarden-bin github-desktop-bin atom-editor-bin
+yay -S standardnotes-desktop birdtray-git duckstation-git citra-git minecraft-launcher protontricks winetricks widevine mellowplayer teamviewer foxitreader bitwarden-bin github-desktop-bin atom-editor-bin
 
 #Commands for cleaning left over files from main program install command and AUR install command.
 sudo pacman -Rns $(pacman -Qtdq)
