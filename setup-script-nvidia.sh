bdb7798b36013d1c85ae2371cafcd12cdee523dd
Eric Parsley <56867834+WoofahRayetDev@users.noreply.github.com>
2020-11-19 21:19:30 -0800

    Update setup-script-nvidia.sh

--- setup-script-nvidia.sh
+++ setup-script-nvidia.sh
@@ -9,16 +9,6 @@ chmod +x all-cpu-cores-compiling.sh
 #Command for main programs I use
 sudo pacman -S qt nvidia lib32-nvidia-utils nvidia-utils base-devel timeshift nano gamemode lib32-gamemode wine-staging gparted flatpak snapd thunderbird qbittorrent krita obs-studio kdenlive vlc bleachbit libreoffice-fresh git steam lutris discord telegram-desktop unrar p7zip
 
-#Fully enabling spand
-sudo systemctl enable --now apparmor.service
-sudo systemctl enable --now snapd.apparmor.service
-sudo systemctl unmask snapd.service
-sudo systemctl enable --now snapd.socket
-sudo ln -s /var/lib/snapd/snap /snap
-sudo sudo systemctl unmask snapd.service
-sudo systemctl start snapd.service
-echo "export PATH=\$PATH:\/snap/bin/" | sudo tee -a /etc/profile
-
 #Command for downloading and setting up Proton Updater
 wget https://raw.githubusercontent.com/Termuellinator/ProtonUpdater/master/cproton.sh
 chmod +x cproton.sh
