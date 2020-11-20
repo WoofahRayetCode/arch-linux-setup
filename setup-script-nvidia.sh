9798b2cfd5b9f79c532f228795cbe323a19d3d5f
Eric Parsley <56867834+WoofahRayetDev@users.noreply.github.com>
2020-11-19 21:09:41 -0800

    Update setup-script-nvidia.sh

--- setup-script-nvidia.sh
+++ setup-script-nvidia.sh
@@ -7,7 +7,7 @@ chmod +x all-cpu-cores-compiling.sh
 ./all-cpu-cores-compiling.sh
 
 #Command for main programs I use
-sudo pacman -S qt nvidia lib32-nvidia-utils nvidia-utils base-devel timeshift nano gamemode lib32-gamemode wine-staging gparted flatpak snapd thunderbird qbittorrent krita obs-studio kdenlive vlc bleachbit libreoffice-fresh git steam pcsx2 mgba ppsspp retroarch retroarch-assets-glui retroarch-assets-ozone retroarch-assets-xmb lutris discord telegram-desktop unrar p7zip
+sudo pacman -S qt nvidia lib32-nvidia-utils nvidia-utils base-devel timeshift nano gamemode lib32-gamemode wine-staging gparted flatpak snapd thunderbird qbittorrent krita obs-studio kdenlive vlc bleachbit libreoffice-fresh git steam lutris discord telegram-desktop unrar p7zip
 
 #Fully enabling spand
 sudo systemctl enable --now apparmor.service
@@ -32,7 +32,7 @@ wget https://raw.githubusercontent.com/Termuellinator/ProtonUpdater/master/cprot
 chmod +x cproton.sh
 
 #Command for programs I use from the AUR
-yay -S standardnotes-desktop birdtray-git duckstation-git citra-git minecraft-launcher protontricks winetricks widevine mellowplayer teamviewer foxitreader bitwarden-bin github-desktop-bin atom-editor-bin
+yay -S standardnotes-desktop birdtray-git duckstation-git pcsx2-bit ppsspp-git mgba-git citra-git retroarch-git retroarch-assets-git retroarch-assets-xmb-git minecraft-launcher protontricks winetricks widevine mellowplayer teamviewer foxitreader bitwarden-bin github-desktop-bin atom-editor-bin
 
 #Commands for cleaning left over files from main program install command and AUR install command.
 sudo pacman -Rns $(pacman -Qtdq)
