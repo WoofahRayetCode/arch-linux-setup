0dfdc14f7013a8d495dbbc0456ebaf1f2f49d37f
Eric Parsley <56867834+Woofah@users.noreply.github.com>
2021-02-10 00:26:57 -0800

    Update setup-script-nvidia.sh

--- setup-script-nvidia.sh
+++ setup-script-nvidia.sh
@@ -6,32 +6,28 @@ wget https://raw.githubusercontent.com/WoofahRayetDev/general-linux-stuff/master
 chmod +x all-cpu-cores-compiling.sh
 ./all-cpu-cores-compiling.sh
 
-#Set iwd as wifi backend
-wget https://raw.githubusercontent.com/WoofahRayetDev/general-linux-stuff/master/wifi_backend.conf
-mv wifi_backend.conf /etc/NetworkManager/conf.d/
-
 #Remove unwanted programs
 sudo pacman -Rns vim
 
 #Command for main programs I use
-sudo pacman -S iwd qt nvidia nvidia-settings lib32-nvidia-utils nvidia-utils bumblebee primus lib32-primus powerdevil nano gamemode lib32-gamemode wine-staging thunderbird qbittorrent krita obs-studio kdenlive vlc bleachbit libreoffice-fresh git steam lutris discord telegram-desktop ark unrar p7zip
+sudo pacman -S qt nvidia nvidia-settings lib32-nvidia-utils nvidia-utils nano gamemode lib32-gamemode wine-staging thunderbird qbittorrent krita obs-studio vlc bleachbit libreoffice-fresh git steam lutris discord telegram-desktop unrar p7zip
 
 #Remove some unwanted icons from program menu. THANKS OCD :D
 mkdir UnwantedIcons
-sudo mv /usr/share/applications/assistant.desktop /home/ericparsley/UnwantedIcons
-sudo mv /usr/share/applications/designer.desktop /home/ericparsley/UnwantedIcons
-sudo mv /usr/share/applications/linguist.desktop /home/ericparsley/UnwantedIcons
-sudo mv /usr/share/applications/qdbusviewer.desktop /home/ericparsley/UnwantedIcons
-sudo mv /usr/share/applications/UserFeedbackConsole.desktop /home/ericparsley/UnwantedIcons
-sudo mv /usr/share/applications/uxterm.desktop /home/ericparsley/UnwantedIcons
-sudo mv /usr/share/applications/xterm.desktop /home/ericparsley/UnwantedIcons
-sudo mv /usr/share/applications/teamviewerapi.desktop /home/ericparsley/UnwantedIcons
-sudo mv /usr/share/applications/lstopo.desktop /home/ericparsley/UnwantedIcons
-sudo mv /usr/share/applications/bssh.desktop /home/ericparsley/UnwantedIcons
-sudo mv /usr/share/applications/bvnc.desktop /home/ericparsley/UnwantedIcons
-sudo mv /usr/share/applications/avahi-discover.desktop /home/ericparsley/UnwantedIcons
-sudo mv /usr/share/applications/electron.desktop /home/ericparsley/UnwantedIcons
-sudo mv /home/ericparsley/.local/share/applications/assistant.desktop /home/ericparsley/UnwantedIcons
+sudo mv /usr/share/applications/assistant.desktop/home/ericparsley/UnwantedIcons
+sudo mv /usr/share/applications/designer.desktop/home/ericparsley/UnwantedIcons
+sudo mv /usr/share/applications/linguist.desktop/home/ericparsley/UnwantedIcons
+sudo mv /usr/share/applications/qdbusviewer.desktop/home/ericparsley/UnwantedIcons
+sudo mv /usr/share/applications/UserFeedbackConsole.desktop/home/ericparsley/UnwantedIcons
+sudo mv /usr/share/applications/uxterm.desktop/home/ericparsley/UnwantedIcons
+sudo mv /usr/share/applications/xterm.desktop/home/ericparsley/UnwantedIcons
+sudo mv /usr/share/applications/teamviewerapi.desktop/home/ericparsley/UnwantedIcons
+sudo mv /usr/share/applications/lstopo.desktop/home/ericparsley/UnwantedIcons
+sudo mv /usr/share/applications/bssh.desktop/home/ericparsley/UnwantedIcons
+sudo mv /usr/share/applications/bvnc.desktop/home/ericparsley/UnwantedIcons
+sudo mv /usr/share/applications/avahi-discover.desktop/home/ericparsley/UnwantedIcons
+sudo mv /usr/share/applications/electron.desktop/home/ericparsley/UnwantedIcons
+sudo mv /home/ericparsley/.local/share/applications/assistant.desktop/home/ericparsley/UnwantedIcons
 
 
 #Yay setup
@@ -48,7 +44,7 @@ wget https://raw.githubusercontent.com/Termuellinator/ProtonUpdater/master/cprot
 chmod +x cproton.sh
 
 #Command for programs I use from the AUR
-yay -S optimus-manager-git optimus-manager-qt-git birdtray-git duckstation pcsx2-git retroarch-git retroarch-assets-git minecraft-launcher protontricks-git winetricks-git mellowplayer-git standardnotes-desktop teamviewer foxitreader bitwarden-bin github-desktop-bin visual-studio-code-bin
+yay -S linux-mainline birdtray-git retroarch-git retroarch-assets-git minecraft-launcher protontricks-git winetricks-git mellowplayer-git standardnotes-desktop teamviewer foxitreader bitwarden-bin github-desktop-bin visual-studio-code-bin
 
 #Fully enable Teamviewer
 sudo teamviewer daemon restart
