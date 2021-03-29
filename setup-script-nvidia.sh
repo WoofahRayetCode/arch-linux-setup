b8ea36543c130ea7b8f40214d3068f6af0e9e072
Eric Parsley <56867834+Woofah@users.noreply.github.com>
2021-03-29 12:02:00 -0700

    Update setup-script-nvidia.sh

--- setup-script-nvidia.sh
+++ setup-script-nvidia.sh
@@ -11,7 +11,7 @@ wget https://raw.githubusercontent.com/Termuellinator/ProtonUpdater/master/cprot
 chmod +x cproton.sh
 
 #Main programs I use
-sudo pacman -S base-devel yay gamemode lib32-gamemode steam wine-staging lutris thunderbird qbittorrent discord telegram-desktop krita libreoffice-fresh vlc bleachbit
+sudo pacman -S base-devel yay libratbag piper gamemode lib32-gamemode steam wine-staging lutris thunderbird qbittorrent discord telegram-desktop krita libreoffice-fresh vlc bleachbit
 
 #AUR Programs I use
 yay -S freedownloadmanager obs-studio-git birdtray-git retroarch-git retroarch-assets-git protontricks-git winetricks-git duckstation-git pcsx2-git rpcs3-git ppsspp-git vbam-git desmume-git citra-qt-git dolphin-emu-git minecraft-launcher foxitreader etcher-bin dxvk minetime-bin standardnotes-bin
