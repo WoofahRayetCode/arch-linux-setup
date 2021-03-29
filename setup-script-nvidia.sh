1eec44efcaa605e1e6169b505b6cdc6a3e4efc72
Eric Parsley <56867834+Woofah@users.noreply.github.com>
2021-03-28 23:55:21 -0700

    Update setup-script-nvidia.sh

--- setup-script-nvidia.sh
+++ setup-script-nvidia.sh
@@ -11,21 +11,13 @@ wget https://raw.githubusercontent.com/Termuellinator/ProtonUpdater/master/cprot
 chmod +x cproton.sh
 
 #Main programs I use
-sudo pacman -S gamemode lib32-gamemode steam wine-staging lutris thunderbird qbittorrent discord telegram-desktop krita libreoffice-fresh vlc bleachbit
-
-#For Spotify in MellowPlayer
-curl -s "https://gitlab.com/ColinDuquesnoy/MellowPlayer/-/raw/master/scripts/install-widevine.sh" | bash
-
-cd
-mkdir Git
-cd Git
-git clone https://aur.archlinux.org/yay-git.git
-cd yay-git
-makepkg -si
+sudo pacman -S yay gamemode lib32-gamemode steam wine-staging lutris thunderbird qbittorrent discord telegram-desktop krita libreoffice-fresh vlc bleachbit
 
 #AUR Programs I use
 yay -S freedownloadmanager obs-studio-git mellowplayer-git noisetorch-git birdtray-git retroarch-git retroarch-assets-git protontricks-git winetricks-git duckstation-git pcsx2-64bit-git rpcs3-git ppsspp-git vita3k-git vbam-git desmume-git dolphin-emu-git citra-qt-git minecraft-launcher foxitreader shotcut-git zoom etcher-bin dxvk minetime-bin standardnotes-bin visual-studio-code-bin github-desktop-bin
 
+#For Spotify in MellowPlayer
+curl -s "https://gitlab.com/ColinDuquesnoy/MellowPlayer/-/raw/master/scripts/install-widevine.sh" | bash
 #Nvidia CUDA Detection
 nvidia-modprobe
 
