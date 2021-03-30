cfd783e1947c99b52e2e603f29f84d9ebde3e65b
Eric Parsley <56867834+Woofah@users.noreply.github.com>
2021-03-29 20:06:41 -0700

    Update setup-script-nvidia.sh

--- setup-script-nvidia.sh
+++ setup-script-nvidia.sh
@@ -11,10 +11,10 @@ wget https://raw.githubusercontent.com/Termuellinator/ProtonUpdater/master/cprot
 chmod +x cproton.sh
 
 #Main programs I use
-sudo pacman -S base-devel yay gamemode lib32-gamemode steam wine-staging lutris qbittorrent discord telegram-desktop krita libreoffice-fresh vlc bleachbit
+sudo pacman -S base-devel yay obs-studio gamemode lib32-gamemode steam wine-staging lutris qbittorrent discord telegram-desktop krita libreoffice-fresh vlc bleachbit
 
 #AUR Programs I use
-yay -S obs-studio-git protontricks-git winetricks-git retroarch-git retroarch-assets-git duckstation-git pcsx2-git rpcs3-git ppsspp-git vbam-git desmume-git citra-qt-git unityhub minecraft-launcher endless-sky steamcmd dxvk mangohud-common lib32-mangohud mangohud freedownloadmanager spotify foxitreader netstandard-targeting-pack-bin dotnet-targeting-pack-bin dotnet-sdk-bin visual-studio-code-bin github-desktop-bin etcher-bin minetime-bin standardnotes-bin davinci-resolve-studio
+yay -S protontricks-git winetricks-git retroarch-git retroarch-assets-git duckstation-git pcsx2-git rpcs3-git ppsspp-git vbam-git desmume-git citra-qt-git unityhub minecraft-launcher endless-sky steamcmd dxvk mangohud-common lib32-mangohud mangohud freedownloadmanager spotify foxitreader netstandard-targeting-pack-bin dotnet-targeting-pack-bin dotnet-sdk-bin visual-studio-code-bin github-desktop-bin etcher-bin minetime-bin standardnotes-bin davinci-resolve-studio
 
 #Simply reboot computer
 sudo reboot
