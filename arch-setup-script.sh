5e4fe2daa7301448d79698de60bc4b110aa9cdd4
Eric Parsley <56867834+Woofah@users.noreply.github.com>
2021-04-05 17:46:08 -0700

    Update arch-setup-script.sh

--- arch-setup-script.sh
+++ arch-setup-script.sh
@@ -14,7 +14,7 @@ chmod +x cproton.sh
 sudo pacman -S base-devel yay gamemode lib32-gamemode steam wine-staging lutris qbittorrent discord telegram-desktop krita libreoffice-fresh vlc bleachbit
 
 #AUR Programs I use
-yay -S protontricks-git winetricks-git retroarch-git retroarch-assets-git duckstation-git pcsx2-git rpcs3-git ppsspp-git vbam-git desmume-git citra-qt-git obs-studio-git unityhub minecraft-launcher steamcmd dxvk mangohud-common lib32-mangohud mangohud freedownloadmanager foxitreader netstandard-targeting-pack-bin dotnet-targeting-pack-bin dotnet-sdk-bin visual-studio-code-bin github-desktop-bin etcher-bin minetime-bin standardnotes-bin davinci-resolve
+yay -S protontricks-git winetricks-git retroarch-git retroarch-assets-git duckstation-git pcsx2-git rpcs3-git ppsspp-git vbam-git desmume-git citra-qt-git obs-studio-git unityhub minecraft-launcher steamcmd dxvk mangohud-common lib32-mangohud mangohud freedownloadmanager foxitreader mullvad-vpn netstandard-targeting-pack-bin dotnet-targeting-pack-bin dotnet-sdk-bin visual-studio-code-bin github-desktop-bin etcher-bin minetime-bin standardnotes-bin davinci-resolve
 
 #Simply reboot computer
 sudo reboot
