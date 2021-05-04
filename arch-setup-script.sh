4da4b5204d7057cb1fa26efb2608d24b86ddf5f0
Eric Parsley <56867834+WoofahRayetCode@users.noreply.github.com>
2021-05-03 23:25:45 -0700

    Update arch-setup-script.sh

--- arch-setup-script.sh
+++ arch-setup-script.sh
@@ -1,11 +1,6 @@
 #!/bin/bash
 set -e
 
-#Fully use CPU for compiling and compressing
-wget https://raw.githubusercontent.com/WoofahRayetDev/general-linux-stuff/master/all-cpu-cores-compiling.sh
-chmod +x all-cpu-cores-compiling.sh
-./all-cpu-cores-compiling.sh
-
 #Command for downloading and setting up Proton Updater
 wget https://raw.githubusercontent.com/Termuellinator/ProtonUpdater/master/cproton.sh
 chmod +x cproton.sh
