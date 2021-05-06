a7300a9a64662c11838a69d144d1b9ac42f8e273
Eric Parsley <56867834+WoofahRayetCode@users.noreply.github.com>
2021-05-05 20:33:17 -0700

    Update arch-setup-script.sh

--- arch-setup-script.sh
+++ arch-setup-script.sh
@@ -6,10 +6,17 @@ wget https://raw.githubusercontent.com/Termuellinator/ProtonUpdater/master/cprot
 chmod +x cproton.sh
 
 #Main programs I use
-sudo pacman -S gamemode lib32-gamemode steam wine-staging winetricks lutris qbittorrent discord krita libreoffice-fresh vlc bleachbit
+sudo pacman -S git nvidia gamemode lib32-gamemode steam wine-staging winetricks lutris qbittorrent discord krita libreoffice-fresh vlc bleachbit
+
+#Install yay
+mkdir Git
+cd Git
+git clone https://aur.archlinux.org/yay.git
+cd yay
+makepkg -si
 
 #AUR Programs I use
-yay -S obs-studio-git protontricks minecraft-launcher steamcmd foxitreader mullvad-vpn bitwarden-bin davinci-resolve vscodium-bin vscodium-bin-marketplace github-desktop-bin etcher-bin minetime-bin standardnotes-bin 
+yay -S duckstation-git pcsx2-git rpcs3-git ppsspp-git vbam-git desmume-git citra-canary-git obs-studio-git protontricks minecraft-launcher steamcmd foxitreader mullvad-vpn bitwarden-bin davinci-resolve vscodium-bin vscodium-bin-marketplace github-desktop-bin etcher-bin minetime-bin standardnotes-bin 
 
 #Simply reboot computer
 sudo reboot
