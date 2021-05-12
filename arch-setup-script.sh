a87f352b3dc59f84e4d9f0d2e73b4b18e31e566c
Eric Parsley <56867834+WoofahRayetCode@users.noreply.github.com>
2021-05-12 15:18:35 -0700

    Update arch-setup-script.sh

--- arch-setup-script.sh
+++ arch-setup-script.sh
@@ -6,19 +6,15 @@ wget https://raw.githubusercontent.com/WoofahRayetCode/arch-linux-stuff/master/a
 chmod +x all-cpu-cores-compiling.sh
 ./all-cpu-cores-compiling.sh
 
-#Command for downloading and setting up Proton Updater
+#Command for downloading and setting up Proton Updater for steam
 wget https://raw.githubusercontent.com/Termuellinator/ProtonUpdater/master/cproton.sh
 chmod +x cproton.sh
 
 #Main programs I use
-sudo pacman -S git qt gamemode lib32-gamemode steam wine-staging winetricks lutris qbittorrent discord krita libreoffice-fresh vlc bleachbit spectacle
+sudo pacman -S git qt alacritty gamemode lib32-gamemode steam wine-staging winetricks lutris qbittorrent discord krita libreoffice-fresh vlc bleachbit spectacle
 
-#Install yay
-mkdir Git
-cd Git
-git clone https://aur.archlinux.org/yay.git
-cd yay
-makepkg -si
+#Remove unwanted programs
+sudo pacman -Rns konsole
 
 #AUR Programs I use
 yay -S duckstation-git pcsx2-git rpcs3-git ppsspp-git mgba-git desmume-git citra-git mullvad-vpn protontricks minecraft-launcher steamcmd foxitreader mellowplayer signal-desktop-beta-bin bitwarden-bin vscodium-bin vscodium-bin-marketplace github-desktop-bin etcher-bin minetime-bin standardnotes-bin 
