a218134c4a51d96bb1ab1947bdd7fb4149fb7aa9
Eric Parsley <56867834+WoofahRayetCode@users.noreply.github.com>
2021-05-12 16:19:14 -0700

    Update arch-setup-script.sh

--- arch-setup-script.sh
+++ arch-setup-script.sh
@@ -11,13 +11,10 @@ wget https://raw.githubusercontent.com/Termuellinator/ProtonUpdater/master/cprot
 chmod +x cproton.sh
 
 #Main programs I use
-sudo pacman -S nvidia qt gamemode lib32-gamemode steam wine-staging winetricks lutris qbittorrent discord krita libreoffice-fresh vlc bleachbit spectacle
-
-#Remove unwanted programs
-sudo pacman -Rns konsole
+sudo pacman -S nvidia qt gamemode lib32-gamemode steam wine-staging winetricks lutris qbittorrent discord krita libreoffice-fresh vlc kdenlive bleachbit spectacle
 
 #AUR Programs I use
-yay -S duckstation-git pcsx2-git rpcs3-git ppsspp-git mgba-qt-git desmume-git citra-git protontricks minecraft-launcher steamcmd foxitreader mellowplayer signal-desktop-beta-bin bitwarden-bin vscodium-bin vscodium-bin-marketplace github-desktop-bin etcher-bin minetime-bin standardnotes-bin 
+yay -S duckstation-git pcsx2-git rpcs3-git ppsspp-git mgba-qt-git desmume-git citra-git protontricks minecraft-launcher steamcmd foxitreader mellowplayer signal-desktop-beta-bin bitwarden-bin vscodium-bin vscodium-bin-marketplace github-desktop-bin etcher-bin minetime-bin standardnotes-bin
 
 #Remove icons cuz OCD :s
 cd /usr/share/applications
@@ -28,7 +25,6 @@ sudo rm -f qdbusviewer.desktop
 sudo rm -f uxterm.desktop
 sudo rm -f xterm.desktop
 sudo rm -f UserFeedbackConsole.desktop
-sudo rm -f lstopo.desktop
 cd
 
 #To make sure program can use the Nvidia GPU
