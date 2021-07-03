01020f976b23dabd75e1bd157db6752d6ceb9215
Eric Parsley <56867834+WoofahRayetCode@users.noreply.github.com>
2021-07-03 01:04:59 -0700

    Update arch-setup-script.sh

--- arch-setup-script.sh
+++ arch-setup-script.sh
@@ -10,8 +10,11 @@ chmod +x all-cpu-cores-compiling.sh
 wget https://raw.githubusercontent.com/Termuellinator/ProtonUpdater/master/cproton.sh
 chmod +x cproton.sh
 
+sudo pacman -Rns ksysguard
+sudo pacman -Rns konsole
+
 #Main programs I use
-sudo pacman -S base-devel qt gamemode lib32-gamemode steam wine-staging winetricks lutris qbittorrent discord krita libreoffice-fresh vlc bleachbit
+sudo pacman -S base-devel alacritty qt gamemode lib32-gamemode steam wine-staging winetricks lutris qbittorrent discord krita libreoffice-fresh vlc bleachbit
 
 #AUR Programs I use
 yay -S duckstation-git pcsx2-git rpcs3-git ppsspp-git mgba-qt-git desmume-git citra-qt-git protontricks minecraft-launcher steamcmd signal-desktop-beta-bin bitwarden-bin vscodium-bin vscodium-bin-marketplace github-desktop-bin standardnotes-bin
@@ -29,10 +32,21 @@ sudo rm -f qdbusviewer.desktop
 sudo rm -f uxterm.desktop
 sudo rm -f xterm.desktop
 sudo rm -f UserFeedbackConsole.desktop
+sudo rm -f org.kde.plasma.emojier.desktop
+sudo rm -f avahi-discover.desktop
+sudo rm -f lstopo.desktop
+sudo rm -f qv4l2.desktop
+sudo rm -f qvidcap.desktop
 cd
 
 #To make sure program can use the Nvidia GPU
 nvidia-modprobe
+
+#Clean shit
+sudo pacman -Rns $(pacman -Qtdq)
+sudo pacman -Scc
+yay -Scc
+rm -rf ~/.cache/*
 
 #Restart
 sudo restart
