2d45db636832bf218d7d36aa0e6579670bd3aba1
Eric Parsley <56867834+WoofahRayetCode@users.noreply.github.com>
2021-07-07 01:40:51 -0700

    Forgot to add cd

--- stalkercrashfix-desktop.sh
+++ stalkercrashfix-desktop.sh
@@ -7,6 +7,7 @@ cd
 
 cd /home/ericparsley/GAMES/Steam/steamapps/common/STALKER\ Shadow\ of\ Chernobyl/gamedata
 find -type f -name "Thumbs.db" -exec rm -i {} \;
+cd
 
 cd /home/ericparsley/GAMES/Steam/steamapps/common/STALKER\ Call\ of\ Pripyat/gamedata
 find -type f -name "Thumbs.db" -exec rm -i {} \;
