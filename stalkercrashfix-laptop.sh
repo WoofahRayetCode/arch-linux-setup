7a60f4e55895b4edd50ce431e5f11213820d4298
Eric Parsley <56867834+WoofahRayetCode@users.noreply.github.com>
2021-07-06 19:39:53 -0700

    Made file for laptop because of different folder locations

---
+++ stalkercrashfix-laptop.sh
@@ -0,0 +1,12 @@
+cd /home/ericparsley/.steam/steam/steamapps/common/STALKER\ Clear\ Sky/gamedata
+find -type f -name "Thumbs.db" -exec rm -i {} \;
+cd
+
+cd /home/ericparsley/.steam/steam/steamapps/common/STALKER\ Shadow\ of\ Chernobyl/gamedata
+find -type f -name "Thumbs.db" -exec rm -i {} \;
+cd
+
+cd /home/ericparsley/.steam/steam/steamapps/common/STALKER\ Call\ of\ Pripyat/gamedata
+find -type f -name "Thumbs.db" -exec rm -i {} \;
+cd
+
