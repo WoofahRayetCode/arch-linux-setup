85e12e9526e6fd7877561eb9f8e6cbbc02a801a1
Eric Parsley <56867834+WoofahRayetCode@users.noreply.github.com>
2021-08-15 03:10:47 -0700

    Separate file to remove icons I don't want to see.

---
+++ hide-icons.sh
@@ -0,0 +1,22 @@
+cd /usr/share/applications
+sudo rm -f avahi-discover.desktop
+sudo rm -f bssh.desktop
+sudo rm -f bvnc.desktop
+sudo rm -f assistant.desktop
+sudo rm -f designer.desktop
+sudo rm -f linguist.desktop
+sudo rm -f qdbusviewer.desktop
+sudo rm -f UserFeedbackConsole.desktop
+sudo rm -f org.kde.plasma.emojier.desktop
+sudo rm -f lstopo.desktop
+sudo rm -f qv4l2.desktop
+sudo rm -f qvidcap.desktop
+sudo rm -f electron.desktop
+sudo rm -f org.kde.plasmaengineexplorer-desktop
+sudo rm -f org.kde.plasma.lookandfeelexplorer.desktop
+sudo rm -f org.kde.plasma.themeexplorer.desktop
+sudo rm -f jshell-java-openjdk.desktop
+cd
+cd /home/ericparsley/.local/share/applications
+sudo rm -f jconsole-java-openjdk.desktop
+cd
