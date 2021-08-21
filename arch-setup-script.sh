7435a375750f6a6102a039fd8ce109142e3799a0
Eric Parsley <56867834+WoofahRayetCode@users.noreply.github.com>
2021-08-21 11:35:47 -0700

    Update arch-setup-script.sh

--- arch-setup-script.sh
+++ arch-setup-script.sh
@@ -14,8 +14,11 @@ chmod +x cproton.sh
 wget https://raw.githubusercontent.com/WoofahRayetCode/arch-linux-stuff/master/clean-files.sh
 chmod +x clean-files.sh
 
+#Update mirrors and checks for updates every time I log in
+alias update="sudo pacman-mirrors -f 5 && sudo pacman -Syyu"
+
 #Main programs I use
-sudo pacman -S base-devel git alacritta qbittorrent electron qt gamemode lib32-gamemode steam wine-staging winetricks lutris discord signal-desktop caprine krita libreoffice-fresh vlc bleachbit
+sudo pacman -S base-devel git alacritty qbittorrent electron qt gamemode lib32-gamemode steam wine-staging winetricks lutris discord signal-desktop caprine krita libreoffice-fresh vlc bleachbit guvcview
 
 #Install yay
 cd
@@ -27,7 +30,7 @@ makepkg -si
 cd
 
 #AUR Programs I use
-yay -S duckstation-git pcsx2-git rpcs3-git ppsspp-git mgba-qt-git desmume-git citra-qt-git protontricks minecraft-launcher steamcmd obs-studio-git davinci-resolve bitwarden-bin visual-studio-code-bin github-desktop-bin standardnotes-bin
+yay -S duckstation-git pcsx2-git rpcs3-git ppsspp-git mgba-qt-git desmume-git citra-qt-git protontricks minecraft-launcher steamcmd obs-studio-git davinci-resolve bitwarden-bin unity-hub visual-studio-code-bin github-desktop-bin standardnotes-bin noisetorch
 
 
 #To make sure program can use the Nvidia GPU
