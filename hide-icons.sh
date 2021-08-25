02a75d7b56f372557c4fc8611328620bd1fe8e1c
Eric Parsley <56867834+WoofahRayetCode@users.noreply.github.com>
2021-08-24 19:10:35 -0700

    Update hide-icons.sh

--- hide-icons.sh
+++ hide-icons.sh
@@ -2,6 +2,8 @@ cd /usr/share/applications
 sudo rm -f avahi-discover.desktop
 sudo rm -f bssh.desktop
 sudo rm -f bvnc.desktop
+sudo rm -f stoken-gui-small.desktop
+sudo rm -f stoken-gui.desktop
 sudo rm -f assistant.desktop
 sudo rm -f designer.desktop
 sudo rm -f linguist.desktop
@@ -16,6 +18,12 @@ sudo rm -f org.kde.plasmaengineexplorer-desktop
 sudo rm -f org.kde.plasma.lookandfeelexplorer.desktop
 sudo rm -f org.kde.plasma.themeexplorer.desktop
 sudo rm -f jshell-java-openjdk.desktop
+sudo rm -f duckstation-nogui.desktop
+sudo rm -f blackmagicraw-player.desktop
+sudo rm -f blackmagicraw-speedtest.desktop
+sudo rm -f com.blackmagicdesign.resolve-Panels-Setup.desktop
+sudo rm -f com.blackmagicdesign.resolve-CaptureLogs.desktop
+sudo rm -f com.blackmagicdesign.resolve-Installer.desktop
 cd
 cd /home/ericparsley/.local/share/applications
 sudo rm -f jconsole-java-openjdk.desktop
