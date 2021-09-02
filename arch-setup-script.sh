e4b2e781f6d8974b4d7cd38ea6f55c1080b795c6
Eric Parsley <56867834+WoofahRayetCode@users.noreply.github.com>
2021-09-02 06:47:25 -0700

    Update arch-setup-script.sh

--- arch-setup-script.sh
+++ arch-setup-script.sh
@@ -21,10 +21,19 @@ chmod +x hide-icons.sh
 #Programs to remove
 sudo pacman -Rns htop kdeconnect gwenview okular ksysguard plasma-systemmonitor 
 #Main programs I use
-sudo pacman -S base-devel git dotnet-sdk yay plasma-systemmonitor qbittorrent electron qt gamemode lib32-gamemode steam wine-staging winetricks lutris discord signal-desktop telegram-desktop caprine krita libreoffice-fresh vlc bleachbit guvcview
+sudo pacman -S base-devel git dotnet-sdk plasma-systemmonitor qbittorrent electron qt gamemode lib32-gamemode steam wine-staging winetricks lutris discord signal-desktop telegram-desktop caprine krita libreoffice-fresh vlc bleachbit guvcview
+
+#To install AUR stuff
+cd
+mkdir Git
+cd Git
+git clone https://aur.archlinux.org/yay.git
+cd yay
+makepkg -si
+cd
 
 #AUR Programs I use
-yay -S duckstation-git pcsx2-git rpcs3-git ppsspp-git mgba-qt-git melonds-git citra-qt-git protontricks minecraft-launcher steamcmd obs-studio-git davinci-resolve etcher bitwarden-bin unityhub visual-studio-code-bin github-desktop-bin standardnotes-bin
+yay -S duckstation-git pcsx2-git rpcs3-git ppsspp-git mgba-qt-git melonds-git citra-qt-git goverlay protontricks minecraft-launcher steamcmd obs-studio-git davinci-resolve etcher bitwarden-bin unityhub visual-studio-code-bin github-desktop-bin standardnotes-bin
 
 #To make sure program can use the Nvidia GPU
 nvidia-modprobe
