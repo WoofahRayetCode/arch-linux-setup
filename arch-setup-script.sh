a2bed9c87e5700d32fe4d901adb1e8cc27ce54e6
Eric Parsley <56867834+WoofahRayetCode@users.noreply.github.com>
2021-09-11 15:54:34 -0700

    Update arch-setup-script.sh

--- arch-setup-script.sh
+++ arch-setup-script.sh
@@ -28,7 +28,7 @@ sudo pacman -Rns plasma-systemmonitor
 sudo pacman -Rns kcalc
 
 #Main programs I use
-sudo pacman -S base-devel git yay plasma-systemmonitor qbittorrent electron qt gamemode lib32-gamemode steam wine-staging winetricks lutris discord signal-desktop telegram-desktop krita libreoffice-fresh vlc bleachbit guvcview
+sudo pacman -S base-devel git yay plasma-systemmonitor qbittorrent electron qt gamemode lib32-gamemode steam wine-staging winetricks lutris discord telegram-desktop krita libreoffice-fresh vlc bleachbit guvcview
 
 #AUR Programs I use
 yay -S duckstation-git pcsx2-git rpcs3-git ppsspp-git mgba-qt-git melonds-git citra-qt-git noisetorch-git freedownloadmanager unityhub goverlay protontricks minecraft-launcher steamcmd obs-studio-git davinci-resolve microsoft-edge-dev-bin signal-desktop-beta-bin etcher-bin bitwarden-bin dotnet-runtime-bin visual-studio-code-bin github-desktop-bin standardnotes-bin
