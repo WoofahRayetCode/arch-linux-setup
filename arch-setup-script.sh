79e4d8878aeab8f2eded11c7cd067cd2bae12fc5
Eric Parsley <56867834+WoofahRayetCode@users.noreply.github.com>
2021-09-11 15:49:55 -0700

    Update arch-setup-script.sh

--- arch-setup-script.sh
+++ arch-setup-script.sh
@@ -31,9 +31,9 @@ sudo pacman -Rns kcalc
 sudo pacman -S base-devel git yay plasma-systemmonitor qbittorrent electron qt gamemode lib32-gamemode steam wine-staging winetricks lutris discord signal-desktop telegram-desktop krita libreoffice-fresh vlc bleachbit guvcview
 
 #AUR Programs I use
-yay -S duckstation-git pcsx2-git rpcs3-git ppsspp-git mgba-qt-git melonds-git citra-qt-git goverlay protontricks minecraft-launcher steamcmd microsoft-edge-dev-bin obs-studio-git davinci-resolve etcher-bin bitwarden-bin unityhub dotnet-core-bin visual-studio-code-bin github-desktop-bin standardnotes-bin
+yay -S duckstation-git pcsx2-git rpcs3-git ppsspp-git mgba-qt-git melonds-git citra-qt-git noisetorch-git freedownloadmanager unityhub goverlay protontricks minecraft-launcher steamcmd obs-studio-git davinci-resolve microsoft-edge-dev-bin signal-desktop-beta-bin etcher-bin bitwarden-bin dotnet-runtime-bin visual-studio-code-bin github-desktop-bin standardnotes-bin
 
-#To make sure program can use the Nvidia GPU
+#To make sure programs can use the Nvidia GPU
 nvidia-modprobe
 
 #Clean shit
