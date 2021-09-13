4d4e5b2bf39ff2a05c12beca8d3d63984762a117
Eric Parsley <56867834+WoofahRayetCode@users.noreply.github.com>
2021-09-13 12:55:10 -0700

    Update arch-setup-script.sh

--- arch-setup-script.sh
+++ arch-setup-script.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 set -e
 
-#Completely shit on the cpu
+#Fully utilize the cpu
 wget https://raw.githubusercontent.com/WoofahRayetCode/arch-linux-stuff/master/all-cpu-cores-compiling.sh
 chmod +x all-cpu-cores-compiling.sh
 ./all-cpu-cores-compiling.sh
@@ -18,6 +18,11 @@ chmod +x clean-files.sh
 wget https://raw.githubusercontent.com/WoofahRayetCode/arch-linux-stuff/master/hide-icons.sh
 chmod +x hide-icons.sh
 
+#Change update branch to Unstable
+sudo pacman-mirrors --api --set-branch unstable
+sudo pacman-mirrors --fasttrack 5
+sudo pacman -Syyu
+
 #Remove stuff
 sudo pacman -Rns firefox
 sudo pacman -Rns gwenview
@@ -26,17 +31,20 @@ sudo pacman -Rns htop
 sudo pacman -Rns ksysguard
 sudo pacman -Rns plasma-systemmonitor
 sudo pacman -Rns kcalc
+sudo pacman -Rns kdeconnect
+sudo pacman -Rns yakuake
 
 #Main programs I use
-sudo pacman -S base-devel git yay plasma-systemmonitor qbittorrent electron qt gamemode lib32-gamemode steam wine-staging winetricks lutris discord telegram-desktop krita libreoffice-fresh vlc bleachbit guvcview
+sudo pacman -S base-devel git yay plasma-systemmonitor qbittorrent electron qt gamemode lib32-gamemode steam wine-staging winetricks lutris discord telegram-desktop caprineclear
+krita libreoffice-fresh vlc bleachbit guvcview
 
 #AUR Programs I use
-yay -S duckstation-git pcsx2-git ppsspp-git mgba-qt-git melonds-git freedownloadmanager goverlay protontricks davinci-resolve microsoft-edge-dev-bin signal-desktop-beta-bin etcher-bin bitwarden-bin standardnotes-bin
+yay -S duckstation-git pcsx2-git ppsspp-git mgba-qt-git melonds-git goverlay protontricks davinci-resolve microsoft-edge-dev-bin signal-desktop-beta-bin etcher-bin bitwarden-bin standardnotes-bin
 
 #To make sure programs can use the Nvidia GPU
 nvidia-modprobe
 
-#Clean shit
+#Clean leftover stuff
 ./clean-files.sh
 
 #Hide icons
