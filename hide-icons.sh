cd467858aa9ff32e13188eb1aa8a0eb88d97bb1f
Eric Parsley <56867834+WoofahRayetCode@users.noreply.github.com>
2022-05-13 18:05:34 -0700

    Update hide-icons.sh

--- hide-icons.sh
+++ hide-icons.sh
@@ -1,32 +1,24 @@
-cd /usr/share/applications
-sudo rm -f avahi-discover.desktop
-sudo rm -f bssh.desktop
-sudo rm -f bvnc.desktop
-sudo rm -f stoken-gui-small.desktop
-sudo rm -f stoken-gui.desktop
+#!/bin/bash
+set -e
+
+cd /usr/share/applications/
+sudo rm -rf org.kde.cuttlefish.desktop
+sudo rm -f electron.desktop
+sudo rm -f electron16.desktop
+sudo rm -f org.kde.plasmaengineexplorer.desktop
+sudo rm -f org.kde.plasma.lookandfeelexplorer.desktop
+sudo rm -f org.kde.plasma.themeexplorer.desktop
 sudo rm -f assistant.desktop
 sudo rm -f designer.desktop
 sudo rm -f linguist.desktop
 sudo rm -f qdbusviewer.desktop
-sudo rm -f UserFeedbackConsole.desktop
-sudo rm -f lstopo.desktop
-sudo rm -f qv4l2.desktop
-sudo rm -f qvidcap.desktop
-sudo rm -f electron.desktop
-sudo rm -f electron13.desktop
-sudo rm -f electron11.desktop
-sudo rm -f org.kde.plasma.emojier.desktop
-sudo rm -f org.kde.kuserfeedback-console
-sudo rm -f org.kde.plasmaengineexplorer-desktop
-sudo rm -f org.kde.plasma.lookandfeelexplorer.desktop
-sudo rm -f org.kde.plasma.themeexplorer.desktop
-sudo rm -f jshell-java-openjdk.desktop
-sudo rm -f jconsole-java-openjdk.desktop
+sudo rm -f org.kde.kuserfeedback-console.desktop
 sudo rm -f duckstation-nogui.desktop
-sudo rm -f uxterm.desktop
-sudo rm -f xterm.desktop
-sudo rm -f electron.desktop
-cd
-cd /usr/share/applications
-
-cd
+sudo rm -f bssh.desktop
+sudo rm -f bvnc.desktop
+sudo rm -f qv4l2.desktop
+sudo rm -f avahi-discover.desktop
+sudo rm -f mt32emu-qt.desktop
+sudo rm -f munt.desktop
+sudo rm -f lstopo.desktop
+sudo rm -f org.kde.plasma.emojier.desktop
