b78ecac46287baa6edca5ea0173c2530ca5a0a79
WoofahRayetCode <ericiparsley@hotmail.com>
2022-07-15 00:45:29 -0700

    Gotta have a torrent program.

--- arch-setup.sh
+++ arch-setup.sh
@@ -41,7 +41,7 @@ sudo pacman -Syy
 sudo echo -e "\n[kde-unstable]\nInclude = /etc/pacman.d/mirrorlist" | sudo tee -a /etc/pacman.conf
 sudo pacman -Syy
 
-sudo pacman -S nvidia nvidia-settings qt qt6 gnucash wine-staging winetricks protontricks lutris ttf-liberation timeshift ark bleachbit partitionmanager discord steam telegram-desktop krita libreoffice-fresh kdenlive bluemail vkbasalt gamemode
+sudo pacman -S nvidia nvidia-settings qt qt6 gnucash wine-staging winetricks protontricks lutris ttf-liberation timeshift ark bleachbit partitionmanager discord steam telegram-desktop krita libreoffice-fresh kdenlive bluemail vkbasalt gamemode qbittorrent
 
 yay -S pcsx-redux-git duckstation-git pcsx2-git rpcs3-git ppsspp-git mgba-qt-git melonds-git citra-qt-git dolphin-emu-git yuzu-early-access minecraft-launcher an-anime-game-launcher-bin mangohud lib32-mangohud mangohud-common goverlay-bin replay-sorcery-git xone-dkms obs-studio-git protonup-qt visual-studio-code-bin ventoy-bin authy teamviewer noisetorch jdownloader2 vlc signal-desktop-beta onedrive-abraunegg onedrivegui-git
 
