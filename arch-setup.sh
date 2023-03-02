02ac94aafdd192b20656869ed2ae7d420f6773f3
Eric Parsley <56867834+WoofahRayetCode@users.noreply.github.com>
2023-03-01 18:12:06 -0800

    Update arch-setup.sh

--- arch-setup.sh
+++ arch-setup.sh
@@ -42,7 +42,7 @@ sudo sed -i 's/#Color/Color/g' /etc/pacman.conf
 sudo sed -i 's/VerbosePkgLists/VerbosePkgLists\nILoveCandy/g' /etc/pacman.conf
 
 #Install regularly used apps
-sudo pacman -S bluez bluez-utils bluedevil wine-staging winetricks lutris bleachbit steam discord telegram-desktop caprine signal-desktop krita libreoffice-fresh android-tools
+sudo pacman -S bluez bluez-utils bluedevil partitionmanager wine-staging winetricks lutris bleachbit steam discord telegram-desktop caprine signal-desktop krita libreoffice-fresh android-tools
 
 #For cemu-git
 yay -S zarchive-git
