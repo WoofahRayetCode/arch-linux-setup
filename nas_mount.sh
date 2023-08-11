d2de98059ee694204c3725aa2204371ed2aa421d
WoofahRayetCode <ericiparsley@hotmail.com>
2023-08-10 19:59:44 -0700

    Files no long needed.

--- nas_mount.sh
+++
@@ -1,4 +0,0 @@
-#!/bin/bash
-set -e
-
-sudo mount.cifs //192.168.0.183/Public /home/ericparsley/WDMyCloudNAS -o username=ericparsley,uid=1000,gid=1000,vers=2.0
--- test_help.sh
+++
@@ -1,8 +0,0 @@
-#!/bin/bash
-
-echo StlkrWlf1990 | sudo -S umount -l Games Storage WDMyCloudNAS
-rmdir Games Storage WDMyCloudNAS
-echo StlkrWlf1990 | sudo -S gedit /etc/fstab
-echo StlkrWlf1990 | sudo -S gedit /etc/pacman.conf
-yes | sudo pacman -S kdeconnect ksysguard yakuake htop
-
