54634c061bdeec02aae10b0e5584c198dc340ccd
Eric Parsley <56867834+WoofahRayetCode@users.noreply.github.com>
2023-09-22 21:43:59 -0700

    Update arch-setup.sh

--- arch-setup.sh
+++ arch-setup.sh
@@ -45,7 +45,7 @@ sudo chown -R -v ericparsley:ericparsley /etc/pacman.conf
 sudo pacman-key --recv-key 3056513887B78AEB --keyserver keyserver.ubuntu.com
 sudo pacman-key --lsign-key 3056513887B78AEB
 sudo pacman -U 'https://cdn-mirror.chaotic.cx/chaotic-aur/chaotic-keyring.pkg.tar.zst' 'https://cdn-mirror.chaotic.cx/chaotic-aur/chaotic-mirrorlist.pkg.tar.zst'
-sudo echo "[chaotic-aur]" >> /etc/pacman.conf
+sudo echo "\n\n[chaotic-aur]" >> /etc/pacman.conf
 sudo echo "Include = /etc/pacman.d/chaotic-mirrorlist" >> /etc/pacman.conf
 sudo pacman -Sy
 
