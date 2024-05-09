75e3fc5f4f82fff91df137aca56d0dc700eb4f8f
WoofahRayetCode <ericiparsley@hotmail.com>
2024-05-08 18:31:00 -0700

    Did an whoops

--- noisetorch_autostart.sh
+++ noisetorch_autostart.sh
@@ -1,4 +1,4 @@
 #!/bin/bash
 set -e
 
-noisetorch -s alsa_input.usb-MV-SILICON_fifine_SC3_20190808-00.7.analog-stereo -i -o alsa_input.usb-MV-SILICON_fifine_SC3_20190808-00.7.analog-stereo
+noisetorch -s alsa_input.usb-MV-SILICON_fifine_SC3_20190808-00.analog-stereo -i -o alsa_input.usb-MV-SILICON_fifine_SC3_20190808-00.analog-stereo
