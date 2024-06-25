ca1f310744f94316cfaf237049c19d0750402237
WoofahRayetCode <ericiparsley@hotmail.com>
2024-06-24 21:04:35 -0700

    Corrected the code so it starts propery again at startup.

--- goverlay_intel_power_reading.sh
+++ goverlay_intel_power_reading.sh
@@ -1,6 +1,4 @@
 #!/bin/bash
 set -e
 
-su
-echo 'StlkrWlf1990'
-chmod o+r /sys/class/powercap/intel-rapl:0/energy_uj
+echo StlkrWlf1990 | sudo -S chmod o+r /sys/class/powercap/intel-rapl:0/energy_uj
