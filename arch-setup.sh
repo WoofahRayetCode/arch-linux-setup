df8bda36323917ad09a3b1557e39d257bad88eac
WoofahRayetCode <ericiparsley@hotmail.com>
2024-07-18 00:06:57 -0700

    Nevermind. Envycontrol is the way to go.

--- arch-setup.sh
+++ arch-setup.sh
@@ -69,11 +69,7 @@ yay -S duckstation-git pcsx2-git rpcs3-git ppsspp-git vita3k-git mesen2-git rmg-
 
 #install AUR apps that I use
 sudo pacman -Sy
-yay -S supergfxctl plasma6-applets-supergfxctl betterdiscord-installer mediawriter chatall-bin mullvad-vpn standardnotes-bin stream-overlay obs-backgroundremoval gcdemu xpadneo-dkms minecraft-launcher ventoy-bin protonup-qt youtube-music-bin visual-studio-code-bin timeshift noisetorch-git
-
-#Enable supergfxctl
-sudo systemctl enable supergfxd.service
-sudo systemctl enable supergfxd.service
+yay -S envycontrol betterdiscord-installer mediawriter chatall-bin mullvad-vpn standardnotes-bin stream-overlay obs-backgroundremoval gcdemu xpadneo-dkms minecraft-launcher ventoy-bin protonup-qt youtube-music-bin visual-studio-code-bin timeshift noisetorch-git
 
 #Clean left over files from AUR stuff
 ./clean-files.sh
