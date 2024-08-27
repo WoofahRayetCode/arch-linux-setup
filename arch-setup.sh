80a80343acd8e1b7b72eedcde9e9c82d137cf939
WoofahRayetCode <56867834+WoofahRayetCode@users.noreply.github.com>
2024-08-27 01:12:22 -0700

    Custom OBS for Nvidia encode support.

--- arch-setup.sh
+++ arch-setup.sh
@@ -55,7 +55,7 @@ sudo sed -i 's/VerbosePkgLists/VerbosePkgLists\nILoveCandy/g' /etc/pacman.conf
 sudo pacman -Sy
 
 #Lenovo 9i
-sudo pacman -S base-devel nvidia-dkms lib32-nvidia-utils nvidia-utils nvidia-settings ladspa noise-suppression-for-voice alacritty freedownloadmanager neofetch bash-language-server usbmuxd openh264 movit kdenlive partitionmanager mame-tools lutris qbittorrent lib32-mangohud mangohud goverlay wine-staging vulkan-icd-loader lib32-vulkan-icd-loader lib32-vulkan-mesa-layers vulkan-mesa-layers winetricks bleachbit gamemode steam protontricks discord telegram-desktop krita libreoffice-fresh obs-studio android-tools
+sudo pacman -S base-devel nvidia-dkms lib32-nvidia-utils nvidia-utils nvidia-settings ladspa noise-suppression-for-voice alacritty freedownloadmanager neofetch bash-language-server usbmuxd openh264 movit kdenlive partitionmanager mame-tools lutris qbittorrent lib32-mangohud mangohud goverlay wine-staging vulkan-icd-loader lib32-vulkan-icd-loader lib32-vulkan-mesa-layers vulkan-mesa-layers winetricks bleachbit gamemode steam protontricks discord telegram-desktop krita libreoffice-fresh android-tools
 
 #Enable Chaotic AUR
 sudo chown -R -v ericparsley:ericparsley /etc/pacman.conf
@@ -71,7 +71,7 @@ yay -S duckstation-avx-git pcsx2-avx-git rpcs3-git ppsspp-avx-git vita3k-avx-git
 
 #install AUR apps that I use
 sudo pacman -Sy
-yay -S envycontrol webapp-manager tenacity-git baca-ereader-git coolercontrol betterdiscord-installer mediawriter chatall-bin mullvad-vpn standardnotes-bin stream-overlay obs-backgroundremoval-git gcdemu xpadneo-dkms minecraft-launcher ventoy-bin protonup-qt youtube-music-bin visual-studio-code-bin timeshift noisetorch-git
+yay -S envycontrol webapp-manager tenacity-git baca-ereader-git coolercontrol betterdiscord-installer mediawriter chatall-bin mullvad-vpn standardnotes-bin stream-overlay obs-backgroundremoval-git obs-studio-tytan652  gcdemu xpadneo-dkms minecraft-launcher ventoy-bin protonup-qt youtube-music-bin visual-studio-code-bin timeshift noisetorch-git
 
 #Clean left over files from AUR stuff
 ./clean-files.sh
