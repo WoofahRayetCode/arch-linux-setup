3260909338747f314b002b15e6c6a1943e1e4067
Eric Parsley <56867834+WoofahRayetCode@users.noreply.github.com>
2024-09-11 15:47:32 -0700

    Using Proton VPN now. Removed chaotic aur setup as its included with Garuda KDe Lite.

--- arch-setup.sh
+++ arch-setup.sh
@@ -61,14 +61,6 @@ sudo pacman -S base-devel flatpak nvidia-dkms nvidia-settings nvidia-utils lib32
 flatpak install flathub com.obsproject.Studio
 flatpak install flathub com.github.wwmm.easyeffects
 
-#Enable Chaotic AUR
-sudo chown -R -v ericparsley:ericparsley /etc/pacman.conf
-sudo pacman-key --recv-key 3056513887B78AEB --keyserver keyserver.ubuntu.com
-sudo pacman-key --lsign-key 3056513887B78AEB
-sudo pacman -U 'https://cdn-mirror.chaotic.cx/chaotic-aur/chaotic-keyring.pkg.tar.zst' 'https://cdn-mirror.chaotic.cx/chaotic-aur/chaotic-mirrorlist.pkg.tar.zst'
-sudo echo -e "\n[chaotic-aur]" >> /etc/pacman.conf
-sudo echo -e "Include = /etc/pacman.d/chaotic-mirrorlist" >> /etc/pacman.conf
-
 #Install yay
 sudo pacman -S yay
 
@@ -78,7 +70,7 @@ yay -S duckstation-avx-git pcsx2-avx-git rpcs3-git ppsspp-avx-git vita3k-avx-git
 
 #install AUR apps that I use
 sudo pacman -Sy
-yay -S envycontrol lmstudio-appimage webapp-manager tenacity-git baca-ereader-git coolercontrol standardnotes-bin stream-overlay obs-backgroundremoval-git gcdemu xpadneo-dkms minecraft-launcher ventoy-bin protonup-qt youtube-music-bin vscodium-bin-features vscodium-bin-marketplace
+yay -S envycontrol proton-vpn-gtk-app lmstudio-appimage webapp-manager tenacity-git baca-ereader-git coolercontrol standardnotes-bin stream-overlay obs-backgroundremoval-git gcdemu xpadneo-dkms minecraft-launcher ventoy-bin protonup-qt youtube-music-bin vscodium-bin-features vscodium-bin-marketplace
 
 #Make 4090 work at full power
 sudo systemctl enable nvidia-powerd.service
