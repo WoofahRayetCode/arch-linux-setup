99e64c38a06bd5be4793a02eb3177f820e60e211
Eric Parsley <56867834+WoofahRayetCode@users.noreply.github.com>
2024-10-11 22:50:00 -0700

    Update arch-setup.sh

--- arch-setup.sh
+++ arch-setup.sh
@@ -6,13 +6,17 @@ sudo micro /etc/makepkg.conf
 
 #Enable multilib and add chaotic aur
 sudo micro /etc/pacman.conf
+
+sudo pacman-key --recv-key 3056513887B78AEB --keyserver keyserver.ubuntu.com
+sudo pacman-key --lsign-key 3056513887B78AEB
+sudo pacman -U 'https://cdn-mirror.chaotic.cx/chaotic-aur/chaotic-keyring.pkg.tar.zst'
+sudo pacman -U 'https://cdn-mirror.chaotic.cx/chaotic-aur/chaotic-mirrorlist.pkg.tar.zst'
+
 [chaotic-aur]
 Include = /etc/pacman.d/chaotic-mirrorlist
 
-pacman-key --recv-key 3056513887B78AEB --keyserver keyserver.ubuntu.com
-pacman-key --lsign-key 3056513887B78AEB
-pacman -U 'https://cdn-mirror.chaotic.cx/chaotic-aur/chaotic-keyring.pkg.tar.zst'
-pacman -U 'https://cdn-mirror.chaotic.cx/chaotic-aur/chaotic-mirrorlist.pkg.tar.zst'
+sudo pacman -Sy
+sudo pacman-key --init
 
 #Enable bluetooth
 sudo systemctl start bluetooth.service
