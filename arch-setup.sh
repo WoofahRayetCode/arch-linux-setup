5d7cfc748c24cfceba669780de9a467f6bfb5cdc
Eric Parsley <56867834+WoofahRayetCode@users.noreply.github.com>
2024-10-11 22:43:18 -0700

    Update arch-setup.sh

--- arch-setup.sh
+++ arch-setup.sh
@@ -4,8 +4,15 @@ set -e
 #Change makeflag so aur programs compiles faster
 sudo micro /etc/makepkg.conf
 
-#Enable multilib
+#Enable multilib and add chaotic aur
 sudo micro /etc/pacman.conf
+[chaotic-aur]
+Include = /etc/pacman.d/chaotic-mirrorlist
+
+pacman-key --recv-key 3056513887B78AEB --keyserver keyserver.ubuntu.com
+pacman-key --lsign-key 3056513887B78AEB
+pacman -U 'https://cdn-mirror.chaotic.cx/chaotic-aur/chaotic-keyring.pkg.tar.zst'
+pacman -U 'https://cdn-mirror.chaotic.cx/chaotic-aur/chaotic-mirrorlist.pkg.tar.zst'
 
 #Enable bluetooth
 sudo systemctl start bluetooth.service
