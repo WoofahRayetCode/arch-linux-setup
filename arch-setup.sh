2ae9968656e3c7a21090e560536001f9377c74c7
Eric Parsley <56867834+WoofahRayetCode@users.noreply.github.com>
2024-10-27 06:01:15 -0700

    Update arch-setup.sh

--- arch-setup.sh
+++ arch-setup.sh
@@ -7,13 +7,13 @@ sudo micro /etc/makepkg.conf
 #Enable multilib and add chaotic aur
 sudo micro /etc/pacman.conf
 
+[chaotic-aur]
+Include = /etc/pacman.d/chaotic-mirrorlist
+
 sudo pacman-key --recv-key 3056513887B78AEB --keyserver keyserver.ubuntu.com
 sudo pacman-key --lsign-key 3056513887B78AEB
 sudo pacman -U 'https://cdn-mirror.chaotic.cx/chaotic-aur/chaotic-keyring.pkg.tar.zst'
 sudo pacman -U 'https://cdn-mirror.chaotic.cx/chaotic-aur/chaotic-mirrorlist.pkg.tar.zst'
-
-[chaotic-aur]
-Include = /etc/pacman.d/chaotic-mirrorlist
 
 sudo pacman -Sy
 sudo pacman-key --init
