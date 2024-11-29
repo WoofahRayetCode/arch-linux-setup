4cd9978ea0249ea290bdfd97c05a0dc0b94e23d3
Eric Parsley <56867834+WoofahRayetCode@users.noreply.github.com>
2024-11-29 06:23:06 -0800

    Update arch_setup.sh

--- arch_setup.sh
+++ arch_setup.sh
@@ -2,7 +2,7 @@
 set -e
 
 #Enable multilib and add chaotic aur
-sudo chown -R -v ericparsley:ericparsley /etc/pacman.conf
+sudo chown -R -v ericiparsley:ericiparsley /etc/pacman.conf
 sudo pacman-key --recv-key 3056513887B78AEB --keyserver keyserver.ubuntu.com
 sudo pacman-key --lsign-key 3056513887B78AEB
 sudo pacman -U 'https://cdn-mirror.chaotic.cx/chaotic-aur/chaotic-keyring.pkg.tar.zst'
@@ -48,8 +48,6 @@ flatpak install flathub io.github.elevenhsoft.WebApps
 flatpak install flathub com.discordapp.Discord
 flatpak install flathub org.kde.krename
 flatpak install flathub com.vscodium.codium
-flatpak install flathub net.cozic.joplin_desktop
-flatpak install flathub com.microsoft.Edge
 
 #Run to get better discord set up
 curl -O https://raw.githubusercontent.com/bb010g/betterdiscordctl/master/betterdiscordctl
