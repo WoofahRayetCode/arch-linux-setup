92210748795483001c5594743146e214f43ac968
Eric Parsley <56867834+WoofahRayetCode@users.noreply.github.com>
2024-11-29 09:18:39 -0800

    Update arch_setup.sh

--- arch_setup.sh
+++ arch_setup.sh
@@ -2,7 +2,7 @@
 set -e
 
 #Enable multilib and add chaotic aur
-sudo chown -R -v ericiparsley:ericiparsley /etc/pacman.conf
+sudo chown -R -v ericparsley:ericparsley /etc/pacman.conf
 sudo pacman-key --recv-key 3056513887B78AEB --keyserver keyserver.ubuntu.com
 sudo pacman-key --lsign-key 3056513887B78AEB
 sudo pacman -U 'https://cdn-mirror.chaotic.cx/chaotic-aur/chaotic-keyring.pkg.tar.zst'
