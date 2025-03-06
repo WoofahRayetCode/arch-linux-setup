fd1424c0de25a7d3a452f3d5ace87609f1a0728b
Eric Parsley <56867834+WoofahRayetCode@users.noreply.github.com>
2025-03-06 12:03:06 -0800

    Update arch_setup.sh

--- arch_setup.sh
+++ arch_setup.sh
@@ -8,13 +8,8 @@ sudo pacman-key --lsign-key 3056513887B78AEB
 sudo pacman -U 'https://cdn-mirror.chaotic.cx/chaotic-aur/chaotic-keyring.pkg.tar.zst'
 sudo pacman -U 'https://cdn-mirror.chaotic.cx/chaotic-aur/chaotic-mirrorlist.pkg.tar.zst'
 sudo pacman-key --init
-sudo echo -e "\n" >> /etc/pacman.conf
 sudo echo -e "\n[chaotic-aur]" >> /etc/pacman.conf
 sudo echo -e "Include = /etc/pacman.d/chaotic-mirrorlist" >> /etc/pacman.conf
-
-#Enable bluetooth
-sudo systemctl start bluetooth.service
-sudo systemctl enable bluetooth.service
 
 #Enable parallel downloading
 sudo sed -i 's/#ParallelDownloads/ParallelDownloads/g' /etc/pacman.conf
@@ -26,13 +21,10 @@ sudo sed -i 's/#Color/Color/g' /etc/pacman.conf
 sudo sed -i 's/VerbosePkgLists/VerbosePkgLists\nILoveCandy/g' /etc/pacman.conf
 
 #Install regularly used apps
-sudo pacman -Sy base-devel linux-headers jq partitionmanager nvidia-dkms nvidia-settings nvidia-utils lib32-nvidia-utils envycontrol switcheroo-control ntfs-3g jre-openjdk flatpak bash-language-server usbmuxd mame-tools lib32-mangohud mangohud goverlay vulkan-icd-loader lib32-vulkan-icd-loader lib32-vulkan-mesa-layers vulkan-mesa-layers winetricks protontricks bleachbit gamemode lib32-gamemode steam android-tools
+sudo pacman -Sy base-devel linux-headers jq partitionmanager ntfs-3g jre-openjdk flatpak bash-language-server usbmuxd mame-tools wine-staging lib32-mangohud mangohud goverlay vulkan-icd-loader lib32-vulkan-icd-loader lib32-vulkan-mesa-layers vulkan-mesa-layers winetricks protontricks bleachbit gamemode lib32-gamemode steam android-tools libreoffice-fresh
 
 #Needed for UUP Dump ISO creation
 sudo pacman -S cabextract wimlib chntpw cdrtools aria2
-
-#Needed to start switchroo service right away
-sudo systemctl enable --now switcheroo-control
 
 #Flatpak apps
 flatpak install flathub com.github.wwmm.easyeffects
@@ -46,18 +38,4 @@ yay -Sy duckstation-git pcsx-redux-git pcsx2-git play-emu rpcs3-git shadps4-git 
 
 #install AUR apps that I use
 sudo pacman -Sy
-yay -Sy peazip msty-bin proton-ge-custom-bin umu-launcher sideloader-bin idevicerestore xpadneo-dkms ventoy-bin zenergy-git
-
-#Full power for Nvidia GPUs - KEEP THIS!
-sudo systemctl enable nvidia-powerd.service
-sudo systemctl start nvidia-powerd.service
-
-#Clean left over files
-wget https://raw.githubusercontent.com/WoofahRayetCode/arch-linux-setup/refs/heads/master/clean_files.sh
-chmod +x clean_files.sh
-sudo ./clean_files.sh
-
-#Hide icons for program I dont use
-wget https://raw.githubusercontent.com/WoofahRayetCode/arch-linux-setup/refs/heads/master/hide_icons.sh
-sudo chmod +x hide_icons.sh
-sudo ./hide_icons.sh
+yay -Sy peazip msty-bin sideloader-bin idevicerestore xpadneo-dkms ventoy-bin visual-studio-code-bin
